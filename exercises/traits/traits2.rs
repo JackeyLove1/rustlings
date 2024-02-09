58bb33a2a4c0334aa3a853f81a5fa1fc56722155
fanhuanjie <fanhuanjie@bytedance.com>
2024-02-09 23:26:35 +0800

    chore: traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,13 +11,19 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut v = self;
+        for it in v.iter_mut() {
+            *it += "Bar";
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
